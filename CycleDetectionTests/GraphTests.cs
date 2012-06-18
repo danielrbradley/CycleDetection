fe2e728c5451b5bf9dbfb6f36b33930709f7d964
daniel <daniel@danielbradley.net>
2012-06-18 16:02:43 +0100

    Add test project.

---
+++ CycleDetectionTests/GraphTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CycleDetection.Tests
+{
+    [TestClass]
+    public class GraphTests
+    {
+        [TestMethod]
+        public void EmptyGraph()
+        {
+            Graph graph = new Graph();
+            var detector = new CycleDetector();
+            var cycles = detector.DetectCycle(graph);
+            Assert.AreEqual(0, cycles.Count);
+        }
+    }
+}
