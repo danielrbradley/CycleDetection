41a833c6c5cbdfb07e54c810ef9d18fbaa65d4c1
daniel <daniel@danielbradley.net>
2012-06-18 16:31:19 +0100

    Correct initial vertex index.

--- CycleDetection/Vertex.cs
+++ CycleDetection/Vertex.cs
@@ -10,16 +10,18 @@ namespace CycleDetection
         public Vertex()
         {
             this.Dependencies = new List<Vertex>();
+            this.Index = -1;
         }
 
         public Vertex(IEnumerable<Vertex> dependencies)
         {
             this.Dependencies = dependencies.ToList();
+            this.Index = -1;
         }
 
-        public int Index { get; set; }
+        internal int Index { get; set; }
 
-        public int LowLink { get; set; }
+        internal int LowLink { get; set; }
 
         public ICollection<Vertex> Dependencies { get; set; }
     }
