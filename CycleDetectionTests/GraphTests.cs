1cb39e0b25df31b6713548d6e93a9b08fc58fa27
daniel <daniel@danielbradley.net>
2012-06-18 16:13:39 +0100

    Add several simple test cases

--- CycleDetectionTests/GraphTests.cs
+++ CycleDetectionTests/GraphTests.cs
@@ -12,10 +12,67 @@ namespace CycleDetection.Tests
         [TestMethod]
         public void EmptyGraph()
         {
-            Graph graph = new Graph();
+            var graph = new Graph();
             var detector = new CycleDetector();
             var cycles = detector.DetectCycle(graph);
             Assert.AreEqual(0, cycles.Count);
+        }
+
+        [TestMethod]
+        public void SingleVertexGraph()
+        {
+            var graph = new Graph();
+            graph.Vertices.Add(new Vertex());
+            var detector = new CycleDetector();
+            var cycles = detector.DetectCycle(graph);
+            Assert.AreEqual(0, cycles.Count);
+        }
+
+        [TestMethod]
+        public void K2NoSimple()
+        {
+            var graph = new Graph();
+            var vA = new Vertex();
+            var vB = new Vertex();
+            vA.Dependencies.Add(vB);
+            graph.Vertices.Add(vA);
+            graph.Vertices.Add(vB);
+            var detector = new CycleDetector();
+            var cycles = detector.DetectCycle(graph);
+            Assert.AreEqual(0, cycles.Count);
+        }
+
+        [TestMethod]
+        public void K2Cycle()
+        {
+            var graph = new Graph();
+            var vA = new Vertex();
+            var vB = new Vertex();
+            vA.Dependencies.Add(vB);
+            vB.Dependencies.Add(vA);
+            graph.Vertices.Add(vA);
+            graph.Vertices.Add(vB);
+            var detector = new CycleDetector();
+            var cycles = detector.DetectCycle(graph);
+            Assert.AreNotEqual(0, cycles.Count);
+        }
+
+        [TestMethod]
+        public void C3()
+        {
+            var graph = new Graph();
+            var vA = new Vertex();
+            var vB = new Vertex();
+            var vC = new Vertex();
+            vA.Dependencies.Add(vB);
+            vB.Dependencies.Add(vC);
+            vC.Dependencies.Add(vA);
+            graph.Vertices.Add(vA);
+            graph.Vertices.Add(vB);
+            graph.Vertices.Add(vC);
+            var detector = new CycleDetector();
+            var cycles = detector.DetectCycle(graph);
+            Assert.AreNotEqual(0, cycles.Count);
         }
     }
 }
